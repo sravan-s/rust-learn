87332c7c6d79bdd623d32d366770839317a725ae
ess_ess <sravans2011@gmail.com>
2016-09-18 23:51:14 +0530

    Type saftey worked out, but issues with loop inside loops

--- sort/src/insertion_sort/mod.rs
+++ sort/src/insertion_sort/mod.rs
@@ -1,4 +1,18 @@
 pub fn sort(array_to_sort: &mut [i8]) {
+  let mut temp: i8;
+  println!("length: {}", array_to_sort.len());
+  for j in 2..array_to_sort.len() {
+    println!("j: {}", j);
+    for k in (1..(j - 1)).rev(){
+      println!("j: {} and k: {}", array_to_sort[j], array_to_sort[k]);
+      println!("k: {}", k);
+      if array_to_sort[j] < array_to_sort[k] {
+        temp = array_to_sort[j];
+        array_to_sort[j] = array_to_sort[k];
+        array_to_sort[k] = temp;
+      }
+    }
+  }
   println!("{:?}", array_to_sort);
 }
 
--- sort/src/main.rs
+++ sort/src/main.rs
@@ -1,7 +1,7 @@
 mod insertion_sort;
 
 fn main() {
-  let mut arr: [i8; 4] = [1, 2, 3, 4];
+  let mut arr: [i8; 4] = [2, 1, 8, 4];
   println!("{:?}", arr);
   insertion_sort::welcome();
   insertion_sort::sort(&mut arr);
